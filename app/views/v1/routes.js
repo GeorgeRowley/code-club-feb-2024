1e6472e28e38962de78ad9a56fad80551c1c2ce7
George Rowley <george.rowley@nhsbsa.nhs.uk>
2024-02-28 15:21:47 +0000

    complex route and pages built

--- app/views/v1/routes.js
+++ app/views/v1/routes.js
@@ -2,12 +2,25 @@
 const express = require('express');
 const router = express.Router();
 
+// Basic journey route
 router.post('/basic-routing-radio', (req, res) => {
     let likeDogs = req.session.data['basicDogs']
     if (likeDogs == "yes"){
         res.redirect("basic-like-dogs")
     } else {
         res.redirect("basic-dislike-dogs")
+    }
+});
+
+// Complex journey route
+router.post('/complex-name', (req, res) => {
+    let name = req.session.data['name']
+    if (name == ""){
+        res.redirect("complex-name-error")
+    } else if (name == "bali" || name == "ralph"){
+        res.redirect("clever-dog")
+    } else {
+        res.redirect("complex-finish")
     }
 });
 
